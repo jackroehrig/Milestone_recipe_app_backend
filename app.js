b5da28ac25d95ec586b5d78f16159c3d61200945
unknown <sjxliu8@gmail.com>
2022-02-16 20:06:47 -0600

    joi joi password complexity installed

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const connection = require("./db")
+const connection = require("./db");
+
+//DB connection
+connection();
 
 //Middleware
 app.use(express.json());
 app.use(cors());
 
-
-
-const port = process.env.PORT || 4000
-app.listen(port, () => console.log(`Port ${port} has risen...`))
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Port ${port} has risen...`));
---
+++ models/user.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const userSChema = new mongoose.Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
