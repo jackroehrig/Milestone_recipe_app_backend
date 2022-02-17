4c31e559e92eec85fd64c538970dc3f7e96bfb9c
unknown <sjxliu8@gmail.com>
2022-02-16 21:38:37 -0600

    adding auth login

---
+++ routes/users.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+// const { validate } = require("json-schema");
+const { User, validate } = require("../models/user");
+const bcrypt = require("bcrypt");
+
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validate(req.body);
+    if (error)
+      return res.status(404).send({ message: error.details[0].message });
+
+    const user = await User.findOne({ email: req.body.email });
+    if (user)
+      return res
+        .status(409)
+        .send({ message: "Account already exist with given email" });
+
+    const salt = await bcrypt.genSalt(Number(process.env.SALT));
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+
+    await new User({ ...req.body, password: hashPassword }).save();
+    res.status(201).send({ message: "User successfully created" });
+  } catch (error) {
+    res.status(404).send({ message: "Server Error" });
+  }
+});
+
+module.exports = router;
