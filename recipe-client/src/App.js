e831783ddad7662c555cb679c0796bd69160096f
unknown <sjxliu8@gmail.com>
2022-02-19 08:33:26 -0600

    moved some files around

--- recipe-client/src/App.js
+++ recipe-client/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import Main from "./components/Sign-Up/Main";
+import Main from "./components/Main";
 import Signup from "./components/Sign-Up";
-import Login from "./components/Sign-Up/Login";
+import Login from "./components/Login";
 
 function App() {
   const user = localStorage.getItem("token");
