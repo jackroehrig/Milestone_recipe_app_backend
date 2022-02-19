c0321e09dc05d369dced1d6621ef346a00076e2f
unknown <sjxliu8@gmail.com>
2022-02-19 07:01:42 -0600

    client added

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const connection = require("./db");
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
 
 //DB connection
 connection();
@@ -10,6 +12,10 @@ connection();
 //Middleware
 app.use(express.json());
 app.use(cors());
+
+//Routes
+app.use("/api/users", userRoutes)
+app.listen("/api/auth", authRoutes)
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Port ${port} has risen...`));
